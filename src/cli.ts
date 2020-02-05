752742fdb1b0d28d9f795564cca97eb258964eb0
azu <azuciao@gmail.com>
2020-02-05 23:25:14 +0900

    fix

--- src/cli.ts
+++ src/cli.ts
@@ -3,24 +3,27 @@ import meow from "meow";
 import { restore } from "./restore-source-map-to-file";
 import * as fs from "fs";
 
-const cli = meow(`
+const cli = meow(
+    `
     Usage
-      $ restore-source-map-to-file [source-map-file] --out￿-dir [dir]
+      $ restore-source-map-to-file [source-map-file] --out-dir [dir]
  
     Options
-      ----out￿-dir Output dir
+      --out-dir Output dir
  
     Examples
       $ restore-source-map-to-file main.js.map --out-dir ./main-original
-`, {
-    flags: {
-        outDir: {
-            type: "string"
-        }
-    },
-    autoHelp: true,
-    autoVersion: true
-});
+`,
+    {
+        flags: {
+            outDir: {
+                type: "string"
+            }
+        },
+        autoHelp: true,
+        autoVersion: true
+    }
+);
 
 export const run = async () => {
     const content = fs.readFileSync(cli.input[0], "utf-8");
